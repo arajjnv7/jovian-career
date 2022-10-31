7991b49ffb595a4024aaa6df5212675806f81f8a
Aryan Raj <94434848+arajjnv7@users.noreply.github.com>
2022-10-31 09:48:38 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+SERVICES = [
+  {
+    'id': 1,
+    'title': 'Web Deevelopment',
+    'time': '5 days'
+  },
+  {
+    'id': 2,
+    'title': 'App Deevelopment',
+    'time': '7 days'
+  },
+  {
+    'id': 3,
+    'title': 'Logo Design',
+    'time': '2 days'
+  },
+]
+
+
 @app.route("/")
-def hello_world():
-    return "Hello,World"
+def hello_jovian():
+  return render_template('home.html', service=SERVICES)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
